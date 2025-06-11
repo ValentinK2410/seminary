e1e0b90f8c33edce3ec34368e2be9f7ecb51731a
DXristianin <dxristianin@gmail.com>
2025-06-11 18:48:13 +0300

    Footer update + icon added

--- src/features/footer/Footer.jsx
+++ src/features/footer/Footer.jsx
@@ -5,6 +5,7 @@ import { Icon_phone } from '../../images/Icon_phone';
 import { Icon_facebook } from '../../images/Icon_facebook';
 import { Icon_instagram } from '../../images/Icon_instagram';
 import { Icon_youtube } from '../../images/Icon_youtube';
+import { Icon_x } from '../../images/Icon_x';
 import { Input } from '../../componentLibrary/Input';
 
 export const Footer = () => {
@@ -12,7 +13,7 @@ export const Footer = () => {
     <footer className="bg-indigo-25 px-6 sm:px-20">
       <div className="w-full max-w-7xl mx-auto flex flex-wrap justify-betwee gap-8 py-[100px]">
         {/* Первая колонка (33.33% на десктопе) */}
-        <div className="w-full lg:w-[calc(33.33%-28px)]">
+        <div className="w-full sm:w-[calc(50%-28px)] lg:w-[calc(33.33%-28px)]">
           <div className='flex gap-2 items-center mb-4 ml-[-12px]'>
             <div className='w-fit min-w-20 ux:min-w-[110px]'>
               <Logo />
@@ -41,7 +42,7 @@ export const Footer = () => {
         </div>
 
         {/* Остальные колонки (22.22% на десктопе) */}
-        <div className="w-full  lg:w-[calc(22.22%-28px)]">
+        <div className="w-full sm:w-[calc(50%-28px)] lg:w-[calc(22.22%-28px)]">
           <h3 className='text-lg font-semibold mb-4'>Быстрые ссылки</h3>
           <ul className='space-y-2'>
             {['О нас', 'Курсы', 'Контакты', 'Блог'].map((item) => (
@@ -55,7 +56,7 @@ export const Footer = () => {
           </ul>
         </div>
 
-        <div className="w-full  lg:w-[calc(22.22%-28px)]">
+        <div className="w-full sm:w-[calc(50%-28px)] lg:w-[calc(22.22%-28px)]">
           <h3 className='text-lg font-semibold mb-4'>Социальные сети</h3>
           <ul className='space-y-2'>
             {['Facebook', 'Twitter', 'Instagram', 'LinkedIn'].map((social) => (
@@ -69,25 +70,25 @@ export const Footer = () => {
           </ul>
         </div>
 
-        <div className="w-full  lg:w-[calc(22.22%-28px)]">
+        <div className="w-full sm:w-[calc(50%-28px)] lg:w-[calc(22.22%-28px)]">
           <h3 className='text-lg font-semibold mb-4'>Подписка на новости</h3>
           <p className='text-zinc-600 mb-4'>
             Подпишитесь на нашу рассылку, чтобы получать последние новости и обновления.
           </p>
-          <form className='space-y-3'>
+          <form className='space-y-3 flex flex-col gap-3'>
             <Input 
               type="email" 
               placeholder="Ваш email" 
-              className='w-full'
+              className='w-full max-h-[50px]'
             />
 
-            <PrimaryButton className='w-full !justify-center'>
+            <PrimaryButton className='w-full !justify-center !m-0'>
               Подписаться
             </PrimaryButton>
           </form>
         </div>
       </div>
-      <div className="w-full max-w-7xl mx-auto flex flex-col md:flex-row gap-6 justify-between border-t border-zinc-200 p-6 text-left text-zinc-600 px-6 sm:px-20">
+      <div className="w-full max-w-7xl mx-auto flex flex-col md:flex-row gap-6 justify-between border-t border-zinc-200 py-6 text-left text-zinc-600">
         <p>&copy; {new Date().getFullYear()} Московская Богословская Семинария. Все права защищены.</p>
         <div className="social-copyright">
           <ul className="flex space-x-4 ml-auto">
@@ -103,7 +104,12 @@ export const Footer = () => {
             </li>
             <li>
               <a href="#" aria-label="YouTube">
-                <Icon_youtube className="w-6 h-6" />
+              <Icon_youtube className="w-6 h-6" />
+              </a>
+            </li>
+             <li>
+              <a href="#" aria-label="X">
+              <Icon_x className="w-6 h-6" />
               </a>
             </li>
           </ul>
