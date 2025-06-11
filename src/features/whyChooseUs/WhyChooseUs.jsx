84d290438a97db1e019d39a71fa857a2a823ac75
Abdul_Ghafur <jdoe@inlichtingenbureau.nl>
2025-06-11 11:20:58 +0200

    add cards to whychooseus

--- src/features/whyChooseUs/WhyChooseUs.jsx
+++ src/features/whyChooseUs/WhyChooseUs.jsx
@@ -1,24 +1,26 @@
 import { LightBulbTopic } from '../../components/LightBulbTopic';
 import { SecondaryButton } from '../../componentLibrary/SecondaryButton'
+import { CardGrid } from './components/CardGrid';
 
 export const WhyChooseUs = () => {
   return (
-    <section className=' bg-blue-800  p-6 sm:p-20  flex flex-col gap-20'>
+    <section className=' bg-blue-800  p-6 sm:p-20 flex flex-col xl:flex-row gap-10 xl:items-center'>
       <div className='w-full max-w-7xl mx-auto'>
         <div className='space-y-5'>
           <LightBulbTopic topic='Почему стоит выбрать МБС?' textColor="text-yellow-300" />
 
-          <h3 className='text-2xl sm:text-4xl text-white max-w-[29ch]'>
+          <h3 className='text-2xl sm:text-4xl text-white md:max-w-[29ch]'>
           Вы определяете цели в соответствии с вашим призванием, а мы помогаем вам выстроить ваш индивидуальный путь обучения
           </h3>  
-          <p className='text-zinc-100 max-w-[70ch]'>
+          <p className='text-zinc-100 md:max-w-[70ch]'>
             Путь служения требует разных знаний и компетенций. Меняются вызовы и контекст. Сотрудники Московской богословской семинарии МБС постоянно анализируют эти вызовы, чтобы помочь вам приобретать те знания и компетенции, которые помогут вам в служении достигать поставленные цели.
           </p>
         </div>
         <SecondaryButton className='mt-10'>
-          Каталог курсов  <span className="ml-2 mt-1 transition-colors duration-200">→</span>
+          Регистрация <span className="ml-2 mt-1 transition-colors duration-200">→</span>
         </SecondaryButton>
       </div>
+      <CardGrid />
     </section>
   )
 }
---
+++ src/features/whyChooseUs/components/CardGrid.jsx
@@ -0,0 +1,42 @@
+export const CardGrid = () => {
+const cards = [
+    { 
+      title: 'Лучшее преподаватели', 
+      icon: '📚', 
+      description: 'Наши преподаватели — эксперты с опытом в богословии. Они поддерживают вас на пути обучения и служения.'
+    },
+    { 
+      title: 'Интерактивное обучение', 
+      icon: '🎓', 
+      description: 'Занятия проходят в динамичной и вовлекающей форме. Вы сможете применять знания на практике сразу.'
+    },
+    { 
+      title: 'Удобное обучение', 
+      icon: '📊', 
+      description: 'Гибкий график позволяет учиться в удобное для вас время. Доступные материалы онлайн. '
+    },
+    { 
+      title: 'Индивидуальный график', 
+      icon: '💻', 
+      description: 'Создайте свой уникальный план обучения с нашей поддержкой. Мы учитываем ваши цели.'
+    },
+  ];
+
+
+  return (
+    <div className="w-fit h-fit rounded-xl grid xs:grid-cols-2 gap-4 xl:mx-auto max-w-3xl   overflow-auto">
+      {cards.map((card, index) => (
+        <div
+          key={index}
+          className="bg-white h-full max-w-[300px] xl:max-w-[400px] rounded-xl shadow-lg flex flex-col  justify-center p-4 pl-6  hover:bg-blue-200  transition-all duration-300  hover:shadow-xl border border-gray-100 aspect-square min-w-0 flex-shrink"
+        >
+          <article className="w-full flex gap-2 justify-start items-center">
+            <div className="text-2xl text-indigo-600">{card.icon}</div>
+            <h3 className="my-3 text-md font-semibold text-gray-900 px-3 leading-tight">{card.title}</h3>
+          </article>
+          <p className="text-left h-fit text-gray-900 leading-tight">{card.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
