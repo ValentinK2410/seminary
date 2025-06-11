190c8fb3b96c241a8bcc816a6722a862bfdc4f78
Abdul_Ghafur <jdoe@inlichtingenbureau.nl>
2025-06-11 11:36:09 +0200

    add cards to whychooseus

--- src/features/whyChooseUs/components/CardGrid.jsx
+++ src/features/whyChooseUs/components/CardGrid.jsx
@@ -24,17 +24,17 @@ const cards = [
 
 
   return (
-    <div className="w-fit h-fit rounded-xl grid xs:grid-cols-2 gap-4 xl:mx-auto max-w-3xl   overflow-auto">
+    <div className="w-fit h-fit xs:p-2 rounded-xl grid xs:grid-cols-2 gap-4 xl:mx-auto max-w-3xl   overflow-auto">
       {cards.map((card, index) => (
         <div
           key={index}
-          className="bg-white h-full max-w-[300px] xl:max-w-[400px] rounded-xl shadow-lg flex flex-col  justify-center p-4 pl-6  hover:bg-blue-200  transition-all duration-300  hover:shadow-xl border border-gray-100 aspect-square min-w-0 flex-shrink"
+          className="group h-full max-w-[300px] xl:max-w-[400px] rounded-xl shadow-lg flex flex-col  justify-center p-4 pl-6 bg-slate-100 hover:bg-white cursor-pointer transition-all duration-300 xs:hover:scale-105 border border-gray-100 aspect-square min-w-0 flex-shrink"
         >
           <article className="w-full flex gap-2 justify-start items-center">
             <div className="text-2xl text-indigo-600">{card.icon}</div>
-            <h3 className="my-3 text-md font-semibold text-gray-900 px-3 leading-tight">{card.title}</h3>
+            <h3 className="group-hover:text-black my-3 text-md font-semibold text-gray-600 px-3 leading-tight">{card.title}</h3>
           </article>
-          <p className="text-left h-fit text-gray-900 leading-tight">{card.description}</p>
+          <p className="text-left h-fit text-zinc-600 group-hover:text-black transition-all duration-300  leading-tight">{card.description}</p>
         </div>
       ))}
     </div>
